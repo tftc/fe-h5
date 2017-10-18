b8cd6892d9f726a8cfee92265b9628e760ee6935
woshiwanting <smartwanting@gmail.com>
2017-10-18 17:18:52 +0800

    增加创建模板时引导信息

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+	name: {
+		desc: 'package name(will publish to npm as egg-{name})',
+	},
+	pluginName: {
+		desc: 'plugin name will be camelcase',
+		default(vars) {
+			return camelcase(vars.name);
+		},
+		filter: camelcase,
+	},
+	description: {
+		desc: 'plugin description',
+	},
+	author: {
+		desc: 'plugin author',
+	},
+};
+
+function camelcase(str) {
+	return str && str.replace(/[_.\- ]+(\w|$)/g, (m, p1) => p1.toUpperCase());
+}
